02d01c25b8adf45cca4ed5e37769264dadc7d0e6
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-10-13 18:17:11 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class Config:
+    TELEGRAM_BOT_TOKEN = '6427721278:AAG6RbjHgfcK_SseHYkbl-YcDvo_KtbZT0M'
+    API_ID = '14099001'
+    API_HASH = 'd4adbe1673633d1df424ae40b530d02a'
+    MONGODB_URI = 'mongodb+srv://ashwani:ASHWANI@ashwani.iwidhpg.mongodb.net/?retryWrites=true&w=majority'
+    LOGGING_GROUP_ID = 'YOUR_LOGGING_GROUP_ID'
