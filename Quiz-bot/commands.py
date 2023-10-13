a76d64c4975197c4deaa6623c0569c128c59b1de
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-10-14 00:03:25 +0530

    Create commands.py

---
+++ Quiz-bot/commands.py
@@ -0,0 +1,22 @@
+from pyrogram import filters
+
+# Define command functions
+def start_command(client, message):
+    # Implementation of /start command
+    pass
+
+def quiz_command(client, message):
+    # Implementation of /quiz command
+    pass
+
+def create_question_command(client, message):
+    # Implementation of /create_question command
+    pass
+
+def results_command(client, message):
+    # Implementation of /results command
+    pass
+
+def broadcast_command(client, message):
+    # Implementation of /broadcast command
+    pass
