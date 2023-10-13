9e177ad5daa01946072cf16401606825d92ce526
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-10-13 23:47:06 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Quiz-bot',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'python-telegram-bot==13.7',
+        'pymongo==3.12.0',
+
+        # Add any other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'Quiz-bot = Quiz_bot:main',  # Adjust the entry point based on your script structure
+        ],
+    },
+    author='Divyanshu Rana',
+    description='A Telegram quiz bot',
+    url='https://github.com/Ranavanshi/Quiz-bot',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
