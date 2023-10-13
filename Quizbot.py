2095fb703d896244b1f68a168d9b3552798bbb44
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-10-13 19:04:11 +0530

    Update Quizbot.py

--- Quizbot.py
+++ Quizbot.py
@@ -1,18 +1,14 @@
-# your_bot_script.py
+# quiz_bot.py
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from pyrogram import Client, Filters
 from pymongo import MongoClient
-from config import Config
+import config  # Import the configuration module
+import logging
+import json
 
-# Use Config class variables
-TELEGRAM_BOT_TOKEN = Config.TELEGRAM_BOT_TOKEN
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-MONGODB_URI = Config.MONGODB_URI
-LOGGING_GROUP_ID = Config.LOGGING_GROUP_ID
-
-# ... rest of your code ...
+# Set up logging
+logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 # Load questions from the JSON file
 with open('quiz_data.json', 'r') as file:
@@ -20,111 +16,79 @@ with open('quiz_data.json', 'r') as file:
 
 questions = quiz_data.get('questions', [])
 
+# Set up MongoDB connection
+client = MongoClient(config.MONGODB_URI)
+db = client.get_default_database()
+scores_collection = db.scores
+
+# Set up Pyrogram
+app = Client("quiz_bot", api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
+
+# ... (rest of the script remains unchanged) ...
+
+# Load questions from the JSON file
+with open('quiz_data.json', 'r') as file:
+    quiz_data = json.load(file)
+
+questions = quiz_data.get('questions', [])
+
+# Set up MongoDB connection
+MONGODB_URI = 'YOUR_MONGODB_URI'
 client = MongoClient(MONGODB_URI)
 db = client.get_default_database()
 scores_collection = db.scores
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Welcome! This is the Quiz Bot. Type /quiz to start a quiz or /broadcast to send a broadcast message.')
+# Set up Pyrogram
+API_ID = 'YOUR_API_ID'
+API_HASH = 'YOUR_API_HASH'
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
 
-def quiz(update: Update, context: CallbackContext) -> None:
-    try:
-        # Get a random quiz question
-        quiz = random.choice(questions)
-        
-        if quiz:
-            context.user_data['current_quiz'] = quiz
-            update.message.reply_text(f"Question: {quiz['question']}\nOptions: {', '.join(quiz['options'])}")
-        else:
-            update.message.reply_text('No quiz available.')
+app = Client("quiz_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-    except Exception as e:
-        update.message.reply_text(f"Error: {str(e)}")
 
-def answer(update: Update, context: CallbackContext) -> None:
-    try:
-        user_answer = update.message.text.lower()
-        quiz = context.user_data.get('current_quiz')
+# Command: /start
+@app.on_message(Filters.command("start"))
+def start_command(client, message):
+    user_id = message.from_user.id
+    message.reply_text(f"Welcome to the Quiz Bot! Use /quiz to start a quiz.")
 
-        if quiz:
-            correct_answer = quiz['answer'].lower()
 
-            if user_answer == correct_answer:
-                update.message.reply_text('Correct! Well done.')
-                # Increment user score
-                user_id = update.message.from_user.id
-                scores_collection.update_one({'user_id': user_id}, {'$inc': {'score': 1}}, upsert=True)
-            else:
-                update.message.reply_text(f'Incorrect. The correct answer is {correct_answer}.')
+# Command: /quiz
+@app.on_message(Filters.command("quiz"))
+def quiz_command(client, message):
+    # Implementation of quiz functionality
+    pass
 
-            # Remove the current quiz from user_data
-            del context.user_data['current_quiz']
-        else:
-            update.message.reply_text('Please start a quiz with /quiz.')
 
-    except Exception as e:
-        update.message.reply_text(f"Error: {str(e)}")
+# Command: /create_question
+@app.on_message(Filters.command("create_question") & Filters.user("everyone"))
+def create_question_command(client, message):
+    # Implementation of question creation functionality
+    pass
 
-def create_quiz(update: Update, context: CallbackContext) -> None:
-    try:
-        # Manually create a quiz (replace with your own logic)
-        question = "What is the capital of Germany?"
-        options = ["Berlin", "Paris", "Madrid", "Rome"]
-        answer = "Berlin"
 
-        quizzes_collection.insert_one({'question': question, 'options': options, 'answer': answer})
-        update.message.reply_text('Quiz created successfully!')
+# Command: /results
+@app.on_message(Filters.command("results") & Filters.user("everyone "))
+def results_command(client, message):
+    # Implementation of results functionality
+    pass
 
-    except Exception as e:
-        update.message.reply_text(f"Error: {str(e)}")
 
-def broadcast(update: Update, context: CallbackContext) -> None:
-    try:
-        # Check if the sender is authorized to broadcast
-        if update.message.from_user.id == YOUR_ADMIN_USER_ID:
-            message_text = update.message.text
+# Command: /broadcast
+@app.on_message(Filters.command("broadcast") & Filters.user("owner"))
+def broadcast_command(client, message):
+    # Implementation of broadcast functionality
+    pass
 
-            # Broadcast the message to all users
-            all_users = scores_collection.find()
-            for user in all_users:
-                context.bot.send_message(user['user_id'], message_text)
 
-    except Exception as e:
-        update.message.reply_text(f"Error: {str(e)}")
+# Polling for Google search feature
+@app.on_message(Filters.text & Filters.regex(r'^/google'))
+def google_search(client, message):
+    # Implementation of Google search functionality
+    pass
 
-def results(update: Update, context: CallbackContext) -> None:
-    try:
-        user_id = update.message.from_user.id
-        user_score = scores_collection.find_one({'user_id': user_id})
-        score = user_score.get('score') if user_score else 0
 
-        update.message.reply_text(f"Your current score: {score}")
+# Run the bot
+if __name__ == "__main__":
+    app.run()
 
-    except Exception as e:
-        update.message.reply_text(f"Error: {str(e)}")
-
-def description(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('This is a Telegram quiz bot. You can start a quiz with /quiz, create a quiz with /create_quiz, check your score with /results, and send a broadcast with /broadcast.')
-
-def main() -> None:
-    try:
-        updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
-        dp = updater.dispatcher
-
-        # Register command handlers
-        dp.add_handler(CommandHandler("start", start))
-        dp.add_handler(CommandHandler("quiz", quiz))
-        dp.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
-        dp.add_handler(CommandHandler("create_quiz", create_quiz))
-        dp.add_handler(CommandHandler("broadcast", broadcast))
-        dp.add_handler(CommandHandler("results", results))
-        dp.add_handler(CommandHandler("description", description))
-
-        updater.start_polling()
-        updater.idle()
-
-    except Exception as e:
-        logger.error(f"Error in main function: {str(e)}")
-
-if __name__ == '__main__':
-    main()
