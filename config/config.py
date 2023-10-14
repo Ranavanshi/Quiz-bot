5656664d70d5a88569eb355a62098d6a9df9e8ad
Divyanshu Rana <141987294+Ranavanshi@users.noreply.github.com>
2023-10-14 21:52:34 +0530

    Update and rename config.py to config/config.py

--- config/config.py
+++ config/config.py
@@ -5,5 +5,5 @@ class Config:
     API_ID = '14099001'
     API_HASH = 'd4adbe1673633d1df424ae40b530d02a'
     MONGODB_URI = 'mongodb+srv://ashwani:ASHWANI@ashwani.iwidhpg.mongodb.net/?retryWrites=true&w=majority'
-    LOGGING_GROUP_ID = 'YOUR_LOGGING_GROUP_ID'
+    LOGGER_GROUP_ID = 'YOUR_LOGGING_GROUP_ID'
     OWNER_ID = '1995154708'
